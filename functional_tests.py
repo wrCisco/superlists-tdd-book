b1bec9cc24843c61606b88bd831e557b198f2f87
wrCisco <lbjma@tiscali.it>
2018-02-19 00:36:23 +0100

    First commit: first FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from selenium import webdriver
+
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
