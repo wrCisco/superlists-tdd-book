3119161065d22acd08c931851e88019cfbf6f369
wrCisco <lbjma@tiscali.it>
2018-02-23 17:09:22 +0100

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -72,6 +72,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new to-do list
         self.browser.get(self.live_server_url)
