e2c7dee5fd41d1c82139a70b36a1b931b07a3f44
wrCisco <lbjma@tiscali.it>
2018-02-23 21:31:39 +0100

    Add List model and new_list view

--- lists/models.py
+++ lists/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('List', default=None)
+
+
+class List(models.Model):
+    pass
+
