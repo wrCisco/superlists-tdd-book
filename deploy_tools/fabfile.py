acee1571bcda0570d5267d55d69a349f99d9f470
wrCisco <lbjma@tiscali.it>
2018-02-28 01:05:47 +0100

    Fix bug in git clone to make it download in current directory

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -21,7 +21,7 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone {REPO_URL}')
+        run(f'git clone {REPO_URL} .')
     current_commit = local('git log -n1 --format=%H', capture=True)
     run(f'git reset --hard {current_commit}')
 
