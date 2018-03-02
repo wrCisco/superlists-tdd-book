a03a0ed5b39e0a96487bb6769f503462208d0321
wrCisco <lbjma@tiscali.it>
2018-03-03 00:32:19 +0100

    Rename all item input ids and names. Still broken

--- lists/forms.py
+++ lists/forms.py
@@ -6,7 +6,7 @@ from django import forms
 from lists.models import Item
 
 # class ItemForm(forms.Form):
-#     item_text = forms.CharField(
+#     text = forms.CharField(
 #         widget=forms.fields.TextInput(attrs={
 #             'placeholder': 'Enter a to-do item',
 #             'class': 'form-control input-lg',
