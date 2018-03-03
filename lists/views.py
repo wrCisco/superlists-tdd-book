9294b8699a2cd578c2ee1ce62940701cf47a2a3a
wrCisco <lbjma@tiscali.it>
2018-03-03 01:28:55 +0100

    Use form in all views, back to working state

--- lists/views.py
+++ lists/views.py
@@ -12,29 +12,32 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ItemForm()
 
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-
-    return render(request, 'lists/list.html', {'list': list_, 'error': error})
+    return render(request, 'lists/list.html', {'list': list_, 'form': form})
+ 
+#         try:
+#             item = Item(text=request.POST['text'], list=list_)
+#             item.full_clean()
+#             item.save()
+#             return redirect(list_) # redirect(list_) works thanks to the get_absolute_url
+#         except ValidationError:    # function defined in the List model
+#             error = "You can't have an empty list item"
+# 
+#     form = ItemForm()
+#     return render(request, 'lists/list.html', {'list': list_, 'form': form, 'error': error})
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'lists/home.html', {'error': error})
-    return redirect(list_) # This works thanks to the get_absolute_url 
-                           # function defined in the List model
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'lists/home.html', {'form': form})
 
