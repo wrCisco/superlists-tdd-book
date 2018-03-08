0c26b52b5110d5a3901a428269e169cf2e060805
wrCisco <lbjma@tiscali.it>
2018-03-08 01:49:04 +0100

    Add standard Django logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 
 from .views import send_login_email, login
 
 urlpatterns = [
     url(r'^send_login_email$', send_login_email, name='send_login_email'),
     url(r'^login$', login, name='login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
 
